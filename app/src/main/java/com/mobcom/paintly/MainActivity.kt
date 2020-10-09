6ecb8cf2f2169bac4fe84963decdcc560726d23d
Salsa Rahmadati <43996652+salsarahmadati@users.noreply.github.com>
2020-10-10 00:55:47 +0700

    impement profile

--- app/src/main/java/com/mobcom/paintly/MainActivity.kt
+++ app/src/main/java/com/mobcom/paintly/MainActivity.kt
@@ -2,21 +2,33 @@ package com.mobcom.paintly
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import org.jetbrains.anko.button
-import org.jetbrains.anko.editText
-import org.jetbrains.anko.sdk27.coroutines.onClick
-import org.jetbrains.anko.toast
-import org.jetbrains.anko.verticalLayout
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-        verticalLayout {
-            val name = editText()
-            button("Say Hello") {
-                onClick { toast("Hello, ${name.text}!") }
+        setContentView(R.layout.activity_bottomnav)
+
+        val homeFragment = Home()
+        val galleryFragment = Gallery()
+        val profileFragment = Profile()
+
+        setCurretFragment(homeFragment)
+
+        bottomNav.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.menu_home -> setCurretFragment(homeFragment)
+                R.id.menu_gallery -> setCurretFragment(galleryFragment)
+                R.id.menu_profile -> setCurretFragment(profileFragment)
             }
+            true
         }
     }
+
+    private fun setCurretFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_fragment, fragment)
+            commit()
+        }
 }
